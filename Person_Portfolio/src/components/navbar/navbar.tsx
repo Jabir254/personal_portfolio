1447fa7145fafb41c65dfaa9f8a8cbedce4fc2f7
jabir254 <jabirkullow@gmail.com>
2024-01-17 19:13:40 +0300

    Navbar

--- Person_Portfolio/src/components/navbar/navbar.tsx
+++ Person_Portfolio/src/components/navbar/navbar.tsx
@@ -11,8 +11,9 @@ function NavBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#home">Portfolio</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+            <Nav.Link href="Projects">Projects</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
