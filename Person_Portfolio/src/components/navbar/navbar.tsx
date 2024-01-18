f829c36b24b2f7cab8b87c3f0ba7d79bd3894f27
jabir254 <jabirkullow@gmail.com>
2024-01-18 08:28:06 +0300

    updated

--- Person_Portfolio/src/components/navbar/navbar.tsx
+++ Person_Portfolio/src/components/navbar/navbar.tsx
@@ -1,21 +1,24 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import "./src/styles/navbar.css";
 
 function NavBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
-      <Container className="d-flex justify-content-evenly">
-        <Navbar.Brand href="#home">Jabir Kullow</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Portfolio</Nav.Link>
-            <Nav.Link href="#link">About</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-            <Nav.Link href="#Projects">Projects</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+      <Container>
+        <div className="navbar-top d-flex">
+          <Navbar.Brand href="#home">Jabir Kullow</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Portfolio</Nav.Link>
+              <Nav.Link href="#link">About</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+              <Nav.Link href="#Projects">Projects</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
       </Container>
     </Navbar>
   );
