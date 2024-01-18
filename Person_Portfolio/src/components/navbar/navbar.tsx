c0ba0fe65ae0cb0586500fd9ed8ba1a0081fac7c
jabir254 <jabirkullow@gmail.com>
2024-01-18 08:10:12 +0300

    navbar

--- Person_Portfolio/src/components/navbar/navbar.tsx
+++ Person_Portfolio/src/components/navbar/navbar.tsx
@@ -4,18 +4,16 @@ import Navbar from "react-bootstrap/Navbar";
 
 function NavBar() {
   return (
-    <Navbar
-      expand="lg"
-      className="bg-body-tertiary d-flex justify-content-evenly"
-    >
-      <Container>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container className="d-flex justify-content-evenly">
         <Navbar.Brand href="#home">Jabir Kullow</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Portfolio</Nav.Link>
             <Nav.Link href="#link">About</Nav.Link>
-            <Nav.Link href="Projects">Projects</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link href="#Projects">Projects</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
